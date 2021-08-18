5bbfa4ce3e81f241f43a06ecc53ffb98083b62de
Daniel Rosenthal <daniel.b.rosenthal@gmail.com>
2021-08-18 14:54:35 -0400

    commit 2

--- main.py
+++ main.py
@@ -4,24 +4,22 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-import time, webbrowser
+import time
 import random
-
+import urllib
 from time import localtime, strftime
 
-starttime = time.time()
+starttime = time.time() #starts time
 id = 0
-hour = 0
-minute = 0
-second = 0
-date = strftime("%Y-%m-%d", localtime())
-time2 = strftime("%H:%M:%S", localtime())
 while True:
-    id = random.randint(15, 30)
+    id = random.randint(15, 30) #generates random temp
+    date = strftime("%Y-%m-%d", localtime()) #gets current date
+    time2 = strftime("%H:%M:%S", localtime()) #gets current time
     url = "http://ec2-35-80-151-233.us-west-2.compute.amazonaws.com/measurements/" \
           "url_create?instrument_id=2&bmp_temp={0}&email=luthyka@wfu.edu&api_key=" \
-          "s-ZNstQ32Uz3uoSWpPHN&at={1}T{2}".format(id, date, time2)
-    webbrowser.open(url)  # Go to example.com
-    time.sleep(5.0 - ((time.time() - starttime) % 5.0))
+          "s-ZNstQ32Uz3uoSWpPHN&at={1}T{2}".format(id, date, time2) #inserts rand temp, date, time into url
+    urllib.urlopen(url) #opens url silently
+    time.sleep(0.5 - ((time.time() - starttime) % 0.5))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
